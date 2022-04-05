13eba348b37050609e8594728e840d1e17c34538
0xRin <73491185+0xRin@users.noreply.github.com>
2022-04-04 21:24:01 -0400

    Refractored if statement

--- main.js
+++ main.js
@@ -4,5 +4,5 @@ const closeBtn = document.querySelector('.close');
 
 loginBtn.addEventListener('click', () => modal.style.display = 'block');
 closeBtn.addEventListener('click', () => modal.style.display = 'none');
-window.addEventListener('click', (e) => {if (e.target == modal) {modal.style.display = 'none'}});
+window.addEventListener('click', (e) => {e.target == modal ? modal.style.display = 'none': NaN});
 
